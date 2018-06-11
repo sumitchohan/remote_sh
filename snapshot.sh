5132373ed8019e778b72d71cb097c7431d27cddb
Sumit Chohan <sumitchohan@Sumits-MacBook-Pro.local>
2018-06-10 19:19:14 -0500

    sd

--- snapshot.sh
+++ snapshot.sh
@@ -1,3 +1,3 @@
 dt=$(date '+%Y-%m-%dT%H_%M_%S');
 adb shell "screencap -p /sdcard/cap_$dt.png"
-adb pull /sdcard/cap_$dt.png ~/Desktop/ScreenShots/cap_$dt.png
+adb pull /sdcard/cap_$dt.png ~/Desktop/Shared/cap_$dt.png
