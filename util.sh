b1a39f25fa6ae3213a58fedf56393b407e661b9c
Sumit Chohan <chauhansumit@gmail.com>
2018-06-12 15:54:33 -0500

    f

--- util.sh
+++ util.sh
@@ -1,12 +1,12 @@
 # sendMessage "localhost" "8080" "Hello"
 function sendMessage {
-    while read line; do
+    echo "$3" | nc "$1" "$2" |   while read line; do
         if [[ $line == "<EOF>" ]]; then
             break
         else
             echo $line
         fi
-    done <(echo "$3" | nc "$1" "$2")
+    done
 }
 
 # function processMessageFromServer {
