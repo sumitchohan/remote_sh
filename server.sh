338f43392551a046e9180b46535016968f0b96d0
Sumit Chohan <chauhansumit@gmail.com>
2018-06-12 13:31:38 -0500

    Sd

--- server.sh
+++ server.sh
@@ -3,7 +3,7 @@ mkfifo out
 trap "rm -f out" EXIT
 while true
 do
-  cat out | nc -l 1500 >>( # parse the netcat output, to build the answer redirected to the pipe "out".
+  cat out | nc -l 1500 > >( # parse the netcat output, to build the answer redirected to the pipe "out".
     export REQUEST=
     while read line
     do
