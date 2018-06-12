5818d81605fb6e3e477a9559f69ac325b31fae4d
Sumit Chohan <chauhansumit@gmail.com>
2018-06-12 15:08:46 -0500

    df

--- util.sh
+++ util.sh
@@ -1,11 +1,12 @@
 # sendMessage "localhost" "8080" "Hello"
 function sendMessage {
+	crlf=$'\n'
     while read line; do
         if [[ $line == "<EOF>" ]]; then
             break
         else
             echo $line
         fi
-    done < <(echo $3 | nc "$1" "$2" <<< "$*")
+    done < <(echo "$3$crlf" | nc "$1" "$2" <<< "$*")
 }
 
