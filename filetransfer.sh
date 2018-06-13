0fddb9a101038be500325c37b8d192f1787512cf
Sumit Chohan <chauhansumit@gmail.com>
2018-06-13 15:26:35 -0500

    wip

--- filetransfer.sh
+++ filetransfer.sh
@@ -4,13 +4,5 @@ trap "rm -f out" EXIT
 portFile=1501
 while true
 do
-	cat out | nc.traditional -l -p $portFile -q 0 > >( # parse the netcat output, to build the answer redirected to the pipe "out".
-    export REQUEST=
-    while read line
-    do
-		echo " request line  - $line"
-		crlf=$'\n' 
-		sh handler.sh $line > out  
-    done
-  )
+	echo "done" | nc.traditional -l -p $portFile -q 0 > file.dump
 done
