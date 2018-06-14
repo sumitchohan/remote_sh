6d160bb887c2ba86f8e746003b7a716314b47b71
Sumit Chohan <chauhansumit@gmail.com>
2018-06-14 09:03:41 -0500

    wip

--- capture.sh
+++ capture.sh
@@ -2,11 +2,11 @@ adb shell screencap /sdcard/scr.raw
 adb pull /sdcard/scr.raw
 
 // remove the header
-tail -c +13 scr. > scr.rgba
+tail -c +13 scr.raw > scr.rgba
 
 // extract width height and pixelformat:
-hexdump -e '/4 "%d"' -s 0 -n 4 raw.raw
-hexdump -e '/4 "%d"' -s 4 -n 4 raw.raw
-hexdump -e '/4 "%d"' -s 8 -n 4 raw.raw
+hexdump -e '/4 "%d"' -s 0 -n 4 scr.raw
+hexdump -e '/4 "%d"' -s 4 -n 4 scr.raw
+hexdump -e '/4 "%d"' -s 8 -n 4 scr.raw
 
 convert -size 800x666 -depth 8 scr.rgba scr.png
