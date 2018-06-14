e68fa556dd1a89521fffc51c521cbbbb410edacf
Sumit Chohan <chauhansumit@gmail.com>
2018-06-14 10:56:25 -0500

    wip

--- server.sh
+++ server.sh
@@ -1,7 +1,7 @@
 rm -f out
 mkfifo out
 trap "rm -f out" EXIT
-port=1500
+port=8901
 while true
 do
 	cat out | nc.traditional -l -p $port -q 0 > >( # parse the netcat output, to build the answer redirected to the pipe "out".
--- util.sh
+++ util.sh
@@ -1,14 +1,6 @@
 # sendMessage "localhost" "8080" "Hello"
-function sendMessage {
-    echo "$3" | nc "$1" "$2" |   while read line; do
-        if [[ $line == "<EOF>" ]]; then
-			echo "breaking"
-            break;
-            break;
-        else
-            echo $line
-        fi
-    done 
+sendMessage {
+echo $(echo "$1" | tr 'n' ' ')  | nc "192.168.0.106" "8901"
 }
 
 # function processMessageFromServer {
