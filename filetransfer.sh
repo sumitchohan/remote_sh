ee462df178bdb2094240dce083ffbc87995db930
Sumit Chohan <sumitchohan@Sumits-MacBook-Pro.local>
2018-06-13 19:17:27 -0500

    wip

--- filetransfer.sh
+++ filetransfer.sh
@@ -2,9 +2,10 @@ rm /tmp/file.tmp
 echo "test" > /tmp/test.tmp
 nc localhost 9001 < /tmp/test.tmp
 if [ ! -f /tmp/file.tmp ]; then
-    echo "file transfer server not running"
-else
+  echo "File transfer server not running. Starting.."
 	while true; do
 		nc -l 9001 > /tmp/file.tmp
 	done
+else
+    echo "file transfer server already running"
 fi
