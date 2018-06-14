8e9523db9b0a07af73f5817fed77afb18422c742
Sumit Chohan <chauhansumit@gmail.com>
2018-06-14 15:08:32 -0500

    wip

--- ocr.sh
+++ ocr.sh
@@ -1,5 +1,5 @@
 while read line
 do
 	parts=($(echo "$line" | tr ',' '\n')) 
-	echo "convert 'scr.png[${parts[3]}x${parts[4]}+${parts[2]}+${parts[1]}]' ${parts[0]}.png" | sh
+	echo "convert '/tmp/scr.png[${parts[3]}x${parts[4]}+${parts[2]}+${parts[1]}]' /tmp/${parts[0]}.png" | sh
 done < "$1.config" 
