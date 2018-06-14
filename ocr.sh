9c49f05e3f396da623de99dfcada00a88ba06c06
Sumit Chohan <chauhansumit@gmail.com>
2018-06-14 15:35:01 -0500

    wip

--- ocr.sh
+++ ocr.sh
@@ -2,5 +2,5 @@ while read line
 do
 	parts=($(echo "$line" | tr ',' '\n')) 
 	echo "convert '/tmp/scr.png[${parts[3]}x${parts[4]}+${parts[2]}+${parts[1]}]' /tmp/${parts[0]}.png" | sh
-	echo "tesseract /tmp/${parts[0]}.png -o ${parts[0]}" | sh
+	echo "tesseract /tmp/${parts[0]}.png -o ${parts[0]}" 
 done < "$1.config"  
