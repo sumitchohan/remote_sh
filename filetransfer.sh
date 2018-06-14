5c38fb959fd5607ce44b909e159bea82e2c856d4
Sumit Chohan <sumitchohan@Sumits-MacBook-Pro.local>
2018-06-13 19:14:36 -0500

    wip

--- filetransfer.sh
+++ filetransfer.sh
@@ -1,7 +1,7 @@
 rm /tmp/file.tmp
 echo "test" > /tmp/test.tmp
 nc localhost 9001 < /tmp/test.tmp
-if [ ! /tmp/file.tmp ]; then
+if [ ! -f /tmp/file.tmp ]; then
     echo "file transfer server not running"
 else
 	echo "file transfer server is running"
