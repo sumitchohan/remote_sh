58da55f73da04a4b6e43ba44d1861b9d72f69e0f
Sumit Chohan <chauhansumit@gmail.com>
2018-06-15 15:28:43 -0500

    wip

--- pythonWebServer/pyramidapp.py
+++ pythonWebServer/pyramidapp.py
@@ -12,7 +12,7 @@ def click(request):
     y = request.matchdict.get('y', -1)
     print y
     d.click(int(float(x)),int(float(y)))	
-    return Response('click %(x)s!' % request.matchdict) 
+    return Response('click %(x) %(y)!' % request.matchdict) 
 	
 def drag(request):
     x = request.matchdict.get('x', -1)
@@ -25,8 +25,22 @@ def drag(request):
     print dy
     n = request.matchdict.get('n', -1)
     print n
-    d.click(int(float(x)),int(float(y)))	
-    return Response('click %(x)s!' % request.matchdict) 
+    d.drag(int(float(x)),int(float(y)),int(float(dx)),int(float(dy)),steps=int(float(n)))	
+    return Response('drag %(x) %(y) %(dx) %(dy) %(n)!' % request.matchdict) 
+	
+def swipe(request):
+    x = request.matchdict.get('x', -1)
+    print x
+    y = request.matchdict.get('y', -1)
+    print y
+    dx = request.matchdict.get('dx', -1)
+    print dx
+    dy = request.matchdict.get('dy', -1)
+    print dy
+    n = request.matchdict.get('n', -1)
+    print n
+    d.swipe(int(float(x)),int(float(y)),int(float(dx)),int(float(dy)),steps=int(float(n)))	
+    return swipe('drag %(x) %(y) %(dx) %(dy) %(n)!' % request.matchdict) 
 
 if __name__ == '__main__':
     with Configurator() as config:
@@ -34,6 +48,8 @@ if __name__ == '__main__':
         config.add_view(click, route_name='click')
         config.add_route('drag', '/drag/{x}/{y}/{dx}/{dy}/{n}')
         config.add_view(drag, route_name='drag')
+        config.add_route('swipe', '/swipe/{x}/{y}/{dx}/{dy}/{n}')
+        config.add_view(drag, route_name='swipe')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
     server.serve_forever()
