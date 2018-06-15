1794f9e2bfc00e7379c1f2e54529edea0da6c78b
Sumit Chohan <sumitchohan@Sumits-MacBook-Pro.local>
2018-06-15 00:24:49 -0500

    wip

--- ocr.sh
+++ ocr.sh
@@ -1,6 +1,6 @@
 while read line
 do
 	parts=($(echo "$line" | tr ',' '\n'))
-	echo "convert '/tmp/scr.png[${parts[3]}x${parts[4]}+${parts[2]}+${parts[1]}]' /tmp/${parts[0]}.png" | sh
+	echo "convert '/tmp/scr.png[${parts[3]}x${parts[4]}+${parts[2]}+${parts[1]}]' /tmp/${parts[0]}.png" 
 	#echo "tesseract /tmp/${parts[0]}.png -o ${parts[0]}" | sh
 done < "$1.config"
