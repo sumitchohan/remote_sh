9a66f7890e3a379e02a3a0e6c44c7859d618cfdd
Sumit Chohan <chauhansumit@gmail.com>
2018-06-15 15:08:08 -0500

    wip

--- pythonWebServer/pyramidapp.py
+++ pythonWebServer/pyramidapp.py
@@ -10,11 +10,13 @@ def click_world(request):
     #d.click(250,250)
     this_id = request.matchdict.get('name', -1)
     print this_id
+    this_id1 = request.matchdict.get('name1', -1)
+    print this_id1
     return Response('click %(name)s!' % request.matchdict) 
 
 if __name__ == '__main__':
     with Configurator() as config:
-        config.add_route('click', '/click/{name}')
+        config.add_route('click', '/click/{name}/{name1}')
         config.add_view(click_world, route_name='click')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
