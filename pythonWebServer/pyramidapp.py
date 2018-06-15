3cb6c6da65bae110908d3cb682cc95fe905f4890
Sumit Chohan <chauhansumit@gmail.com>
2018-06-15 14:58:08 -0500

    wip

--- pythonWebServer/pyramidapp.py
+++ pythonWebServer/pyramidapp.py
@@ -3,18 +3,13 @@ from pyramid.config import Configurator
 from pyramid.response import Response
 
 
-def hello_world(request):
-    return Response('Hello %(name)s!' % request.matchdict)
-	
-def click(request):
-    return Response('click on  %(x)!' % request.matchdict)
+def click_world(request):
+    return Response('click %(name)s!' % request.matchdict) 
 
 if __name__ == '__main__':
     with Configurator() as config:
-        config.add_route('hello', '/hello/{name}')
-        config.add_view(hello_world, route_name='hello')
-        config.add_route('click', '/xy/{x}')
-        config.add_view(click, route_name='click')
+        config.add_route('click', '/click/{name}')
+        config.add_view(click_world, route_name='click')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
     server.serve_forever()
