91f2459f041275aa63579cdf3a3d0c15665c20f9
Sumit Chohan <chauhansumit@gmail.com>
2018-06-15 12:14:02 -0500

    wip

--- auto.py
+++ auto.py
@@ -10,10 +10,11 @@ def execute():
     print "Connected"
     print "Clicking 100 100"
     d.click(100,100)
-    print "Clicking 100 200"
     d.click(100,200)
-    print "Clicking 100 300"
     d.click(100,300)
+    d.click(200,100)
+    d.click(300,200)
+    d.click(400,300)
 def exit_gracefully(signum, frame):
     print "Gracefully exiting"
     signal.signal(signal.SIGINT, signal.getsignal(signal.SIGINT)) 
