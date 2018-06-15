75218df0c6bba758318c2a638f62733e688b26dd
Sumit Chohan <chauhansumit@gmail.com>
2018-06-15 12:08:42 -0500

    wip

---
+++ auto.py
@@ -0,0 +1,17 @@
+﻿from uiautomator import device as d
+import sys
+import signal
+import random
+ 
+
+def execute():
+    print "Connecting to device..."
+    d.info
+    print "Connected" 
+def exit_gracefully(signum, frame):
+    print "Gracefully exiting"
+    signal.signal(signal.SIGINT, signal.getsignal(signal.SIGINT)) 
+    sys.exit(1)
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, exit_gracefully)
+    execute()
