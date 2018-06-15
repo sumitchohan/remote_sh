6225f755af98282db0e246f0d36f7e37d53762b2
Sumit Chohan <chauhansumit@gmail.com>
2018-06-15 15:16:28 -0500

    wip

--- pythonWebServer/pyramidapp.py
+++ pythonWebServer/pyramidapp.py
@@ -6,18 +6,34 @@ import sys
 import signal
 import random
 
-def click_world(request):
+def click(request):
     x = request.matchdict.get('x', -1)
     print x
     y = request.matchdict.get('y', -1)
     print y
     d.click(int(float(x)),int(float(y)))	
     return Response('click %(x)s!' % request.matchdict) 
+	
+def drag(request):
+    x = request.matchdict.get('x', -1)
+    print x
+    y = request.matchdict.get('y', -1)
+    print y
+    dx = request.matchdict.get('dx', -1)
+    print dx
+    dy = request.matchdict.get('dy', -1)
+    print dy
+    n = request.matchdict.get('n', -1)
+    print n
+    d.click(int(float(x)),int(float(y)))	
+    return Response('click %(x)s!' % request.matchdict) 
 
 if __name__ == '__main__':
     with Configurator() as config:
         config.add_route('click', '/click/{x}/{y}')
-        config.add_view(click_world, route_name='click')
+        config.add_view(click, route_name='click')
+        config.add_route('drag', '/drag/{x}/{y}/{dx}/{dy}/{n}')
+        config.add_view(drag, route_name='drag')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
     server.serve_forever()
