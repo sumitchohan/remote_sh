5582a9d381271b6668ac88b32237ad109bad2c70
Sumit Chohan <chauhansumit@gmail.com>
2018-06-17 13:48:58 -0500

    wip

--- capture.sh
+++ capture.sh
@@ -1,11 +1,11 @@
-echo "time - $(date +%N)"
+echo "time - $(date +%S_%N)"
 adb shell screencap /sdcard/scr.raw
-echo "time - $(date +%N)"
+echo "time - $(date +%S_%N)"
 adb pull /sdcard/scr.raw
-echo "time - $(date +%N)"
+echo "time - $(date +%S_%N)"
 tail -c +13 scr.raw > scr.rgba
 hexdump -e '/4 "%d"' -s 0 -n 4 scr.raw
 hexdump -e '/4 "%d"' -s 4 -n 4 scr.raw
 hexdump -e '/4 "%d"' -s 8 -n 4 scr.raw
 convert -size 1600x1332 -depth 8 scr.rgba scr.png
-echo "time - $(date +%N)"
+echo "time - $(date +%S_%N)"
