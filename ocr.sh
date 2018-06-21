d8bf2aa3f4bf1d3f032e354a1060cb5c097f4e8e
Sumit Chohan <chauhansumit@gmail.com>
2018-06-21 15:53:12 -0500

    wip

--- ocr.sh
+++ ocr.sh
@@ -1,12 +1,11 @@
 while read line
 do
+	echo "" >$1_read.sh
 	parts=($(echo "$line" | tr ',' '\n'))
-	echo "convert '/tmp/scr.png[${parts[3]}x${parts[4]}+${parts[1]}+${parts[2]}]' /tmp/${parts[0]}.png" | sh
-	echo "./ocrutil/OcrUtil KeepPixels /tmp/${parts[0]}.png /tmp/${parts[0]}_C.png ./ocrutil/whitelist.txt" | sh 
+	echo "convert '/tmp/scr.png[${parts[3]}x${parts[4]}+${parts[1]}+${parts[2]}]' /tmp/${parts[0]}.png" >>$1_read.sh
+	echo "./ocrutil/OcrUtil KeepPixels /tmp/${parts[0]}.png /tmp/${parts[0]}_C.png ./ocrutil/whitelist.txt" >>$1_read.sh
 	#echo "convert -resize 200% /tmp/${parts[0]}_C.png /tmp/${parts[0]}_C1.png" | sh 
-	echo "tesseract /tmp/${parts[0]}_C.png /tmp/${parts[0]}_C --tessdata-dir ~/Desktop/gh/remote_sh/tessdata -l coc1" | sh
-	cat "/tmp/${parts[0]}_C.txt" | sed 's/[^0-9]*//g' > /tmp/${parts[0]}.txt
-	res=$(cat  "/tmp/${parts[0]}.txt")
-	echo "${parts[0]} - $res"
-	adb push /tmp/${parts[0]}.txt /sdcard/coc/${parts[0]}.txt
+	echo "tesseract /tmp/${parts[0]}_C.png /tmp/${parts[0]}_C --tessdata-dir ~/Desktop/gh/remote_sh/tessdata -l coc1" >>$1_read.sh
+	echo 'cat "/tmp/${parts[0]}_C.txt" | sed 's/[^0-9]*//g' > /tmp/${parts[0]}.txt' >>$1_read.sh 
+	echo "adb push /tmp/${parts[0]}.txt /sdcard/coc/${parts[0]}.txt" >>$1_read.sh
 done < "$1.config"
