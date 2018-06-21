9c99c87a944fcb1dd29425677d2923bc0490f27b
Sumit Chohan <chauhansumit@gmail.com>
2018-06-21 15:56:21 -0500

    wip

--- ocr.sh
+++ ocr.sh
@@ -1,6 +1,6 @@
+echo "" >$1_read.sh
 while read line
 do
-	echo "" >$1_read.sh
 	parts=($(echo "$line" | tr ',' '\n'))
 	echo "convert '/tmp/scr.png[${parts[3]}x${parts[4]}+${parts[1]}+${parts[2]}]' /tmp/${parts[0]}.png" >>$1_read.sh
 	echo "./ocrutil/OcrUtil KeepPixels /tmp/${parts[0]}.png /tmp/${parts[0]}_C.png ./ocrutil/whitelist.txt" >>$1_read.sh
