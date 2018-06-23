21f6af4a62880b7130c47626f950902195e49115
Sumit Chohan <chauhansumit@gmail.com>
2018-06-22 22:59:48 -0500

    wip

--- cv/find_objects_1.py
+++ cv/find_objects_1.py
@@ -4,17 +4,25 @@ from matplotlib import pyplot as plt
 import datetime
 
 MIN_MATCH_COUNT = 10
-
-img1 = cv2.imread('box.png',0)          # queryImage
-img2 = cv2.imread('box_in_scene.png',0) # trainImage
-
 # Initiate SIFT detector
 sift = cv2.xfeatures2d.SIFT_create()
+f = open('source.txt', "r")
+lines = list(f)
+dictSourcedes={}
+for line in lines:
+    print(line)
+    parts=line.split(',')
+    print(parts[0])    
+    img1 = cv2.imread(parts[1],0)          # queryImage
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(img1,None)
+    dictSourcedes[parts[0]]=des1
+    print(len(des1))       
 
-# find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
 
 print (datetime.datetime.now().strftime("%H:%M:%S.%f"))
+
+img2 = cv2.imread('box_in_scene.png',0) # trainImage
 kp2, des2 = sift.detectAndCompute(img2,None)
 
 FLANN_INDEX_KDTREE = 0
@@ -23,37 +31,22 @@ search_params = dict(checks = 50)
 
 flann = cv2.FlannBasedMatcher(index_params, search_params)
 
-matches = flann.knnMatch(des1,des2,k=2)
+matches = flann.knnMatch(dictSourcedes['src1'],des2,k=2)
 
 # store all the good matches as per Lowe's ratio test.
 good = []
 for m,n in matches:
     if m.distance < 0.7*n.distance:
         good.append(m) 
-if len(good)>MIN_MATCH_COUNT:
-    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+print('good matches - '+repr(len(good)))
+if len(good)>MIN_MATCH_COUNT: 
     dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
     print (datetime.datetime.now().strftime("%H:%M:%S.%f"))
-
-    
-    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-    matchesMask = mask.ravel().tolist()
-
-    h,w = img1.shape
-    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-    dst = cv2.perspectiveTransform(pts,M)
-
-    img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
-
+    print (len(dst_pts))
+    sums=dst_pts.sum(axis=0)
+    x=sums[0][0]/len(dst_pts)
+    y=sums[0][1]/len(dst_pts)
+    print(x)
+    print(y)  
 else:
     print ("Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT))
-    matchesMask = None
-	
-draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                   singlePointColor = None,
-                   matchesMask = matchesMask, # draw only inliers
-                   flags = 2)
-
-img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
-
-plt.imshow(img3, 'gray'),plt.show()
