66f35d9015e70ebcad16277eadb7da2c49672d30
Sumit Chohan <chauhansumit@gmail.com>
2018-06-23 12:38:51 -0500

    wip

--- pyramidapp.py
+++ pyramidapp.py
@@ -5,47 +5,67 @@ from uiautomator import device as d
 import sys
 import signal
 import random
+import time
 
 def click(request):
     x = request.matchdict.get('x', -1)
-    print x
+    print (x)
     y = request.matchdict.get('y', -1)
-    print y
+    print (y)
     d.click(int(float(x)),int(float(y)))	
     return Response('click!' % request.matchdict) 
 	
 def drag(request):
     x = request.matchdict.get('x', -1)
-    print x
+    print (x)
     y = request.matchdict.get('y', -1)
-    print y
+    print (y)
     dx = request.matchdict.get('dx', -1)
-    print dx
+    print (dx)
     dy = request.matchdict.get('dy', -1)
-    print dy
+    print (dy)
     n = request.matchdict.get('n', -1)
-    print n
+    print (n)
     d.drag(int(float(x)),int(float(y)),int(float(dx)),int(float(dy)),steps=int(float(n)))	
     return Response('drag!' % request.matchdict) 
 	
 def swipe(request):
     x = request.matchdict.get('x', -1)
-    print x
+    print (x)
     y = request.matchdict.get('y', -1)
-    print y
+    print (y)
     dx = request.matchdict.get('dx', -1)
-    print dx
+    print (dx)
     dy = request.matchdict.get('dy', -1)
-    print dy
+    print (dy)
     n = request.matchdict.get('n', -1)
-    print n
+    print (n)
     d.swipe(int(float(x)),int(float(y)),int(float(dx)),int(float(dy)),steps=int(float(n)))	
     return Response('swipe!' % request.matchdict) 
+
+def execute(request):
+    ff=request.matchdict.get('action',-1)
+    print(ff)
+    f = open(ff, "r")
+    lines = list(f)
+    for line in lines:
+        print(line)
+        parts=line.split(',')
+        print(parts[0])
+        if parts[0]=='tap':
+            print ('Tapping')
+            d.click(parts[1],parts[2])
+        elif parts[0]=='wait':
+            print ('waiting')
+            time.sleep(parts[1])
+    return Response('executed')
 
 if __name__ == '__main__':
     with Configurator() as config:
         config.add_route('click', '/click/{x}/{y}')
         config.add_view(click, route_name='click')
+        config.add_route('execute', '/execute/{action}')
+        config.add_view(execute, route_name='execute')
         config.add_route('drag', '/drag/{x}/{y}/{dx}/{dy}/{n}')
         config.add_view(drag, route_name='drag')
         config.add_route('swipe', '/swipe/{x}/{y}/{dx}/{dy}/{n}')
