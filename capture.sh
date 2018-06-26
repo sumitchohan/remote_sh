d7b9a9ad9f3625346defe2332f2211a72636e040
Sumit Chohan <chauhansumit@gmail.com>
2018-06-26 10:22:34 -0500

    wip

--- capture.sh
+++ capture.sh
@@ -7,5 +7,6 @@ tail -c +13 scr.raw > scr.rgba
 hexdump -e '/4 "%d"' -s 0 -n 4 scr.raw
 hexdump -e '/4 "%d"' -s 4 -n 4 scr.raw
 hexdump -e '/4 "%d"' -s 8 -n 4 scr.raw
-convert -size 800x666 -depth 8 scr.rgba /tmp/scr.png
+# convert -size 800x666 -depth 8 scr.rgba /tmp/scr.png
+convert -size 1600x1332 -depth 8 scr.rgba /tmp/scr.png
 echo "time - $(date +%S_%N)"
