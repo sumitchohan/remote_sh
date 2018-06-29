85539722e97627b25adeef4c07210c00fa8b80cc
Sumit Chohan <chauhansumit@gmail.com>
2018-06-29 14:11:42 -0500

    wip

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 cd cv
+echo "running find objest server"
 python find_objects_1.py & pid=$!
 echo $pid>/tmp/pid_f_o.txt
 cd ..
+echo "run pyramid app"
 python pyramidapp.py & pid=$!
 echo $pid>/tmp/pid_p_a.txt
+echo "run server.sh"
+source server.sh &pid=$!
+echo $pid > /tmp/pid_s.txt
