c29d299167dde64edd95b989fbb5bcc92a27ea2d
Sumit Chohan <chauhansumit@gmail.com>
2018-07-26 16:14:29 -0500

    wip

--- read_donation.sh
+++ read_donation.sh
@@ -16,7 +16,7 @@ do
 	echo "convert '/tmp/scr.png[${parts[3]}x${parts[4]}+${parts[1]}+${parts[2]}]' /tmp/${parts[0]}.png" | sh
 	#echo "./ocrutil/OcrUtil KeepPixels /tmp/${parts[0]}.png /tmp/${parts[0]}_C.png ./ocrutil/whitelist.txt" | sh 
 	#echo "convert -resize 200% /tmp/${parts[0]}_C.png /tmp/${parts[0]}_C1.png" | sh 
-	echo "tesseract /tmp/${parts[0]}.png /tmp/${parts[0]}_C --tessdata-dir ~/Desktop/gh/remote_sh/tessdata -l coc1" | sh
+	echo "tesseract /tmp/${parts[0]}.png /tmp/${parts[0]}_C" | sh
 	cat "/tmp/${parts[0]}_C.txt" | sed 's/[^0-9]*//g' > /tmp/${parts[0]}.txt
 	res=$(cat  "/tmp/${parts[0]}.txt")
 	echo "${parts[0]} - $res"
