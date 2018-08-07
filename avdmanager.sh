6f32ca09dd7c83964523f53ec2ccd2ad4c93ab56
Sumit Chohan <chauhansumit@gmail.com>
2018-08-07 11:27:57 -0500

    wip

--- avdmanager.sh
+++ avdmanager.sh
@@ -3,6 +3,6 @@ threads=$(wmctrl -lx | grep Nexus5XNew | tr ' ' '\n' | grep 0x)
 while read line; do
 	echo "closing - $line"
 	wmctrl -ic $line
-	sleep 10
+	echo "Closed - $line"
 done < <(echo $threads)
 ~/Android/Sdk/emulator/emulator -avd $1
