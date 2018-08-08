ed67484fc8317bc5ff82740bd20a035a0ceaf2b1
Sumit Chohan <chauhansumit@gmail.com>
2018-08-08 10:48:19 -0500

    wip

--- loop.sh
+++ loop.sh
@@ -2,28 +2,9 @@ counter=1
 while [ "$counter" -le 10000 ] 
 do
 	echo "counter - $counter"
-	source avdmanager.sh Nexus5XNew1
-	sleep 15
-	adb shell "echo 'hi'"
-	adb shell "echo 'hi'"
-	adb shell "echo 'hi'"
-	adb shell "echo 'hi'"
-	sleep 5
-	sh init.sh
-	source start.sh	
 	adb shell "cd sdcard/coc && source do.sh && Run 1"
-	
-	source avdmanager.sh Nexus5XNew2
-	sleep 15
-	adb shell "echo 'hi'"
-	adb shell "echo 'hi'"
-	adb shell "echo 'hi'"
-	adb shell "echo 'hi'"
-	sleep 5
-	sh init.sh
-	source start.sh	
 	adb shell "cd sdcard/coc && source do.sh && Run 2"
-	sleep 2000
+	sleep 2100
 	counter=$((counter+1))
 done
 
