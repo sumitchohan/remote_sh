9d12de3381e9f1bbe3061a51ce5c009ddef25002
sumit chohan <sumit@sumits-MacBook-Pro.local>
2018-08-11 19:40:31 -0500

    wip

--- logremote.sh
+++ logremote.sh
@@ -1,4 +1,4 @@
 dt=$(date '+%Y-%m-%dT%H_%M_%S');
-echo "$dt - $@">>logs.log
+echo "$dt - $@ <br/>">>logs.log
 logs=$(head -n 20 logs.log)
 curl -d "$logs" -X POST https://api.keyvalue.xyz/d291d631/logKey -k -s
