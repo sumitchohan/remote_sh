27900dff4473c328358793624f2f3c72d8b32f97
Sumit Chohan <chauhansumit@gmail.com>
2018-08-16 11:02:33 -0500

    wip

--- do.sh
+++ do.sh
@@ -1067,7 +1067,7 @@ MatchPixel() #x y r g b delta
 IsReadyForAttack()
 {
 	Dump	
-    if [ $(MatchPixel 27 150 64 119 10 100) = "y" ] && [ $(MatchPixel 551 296 64 117 09 100) = "y" ]
+    if [ $(MatchPixel 27 150 64 119 10 100) = "y" ] #&& [ $(MatchPixel 551 296 64 117 09 100) = "y" ]
     then		
 		echo "y"
 	else 
--- loop.sh
+++ loop.sh
@@ -1,10 +1,10 @@
 counter=1
-while [ "$counter" -le 10000 ] 
+while [ "$counter" -le 100000 ] 
 do
 	echo "counter - $counter"
 	adb shell "cd sdcard/coc && source do.sh && Run 1"
 	adb shell "cd sdcard/coc && source do.sh && Run 2"
-	sleep 2100
+	sleep 1500
 	counter=$((counter+1))
 done
 
