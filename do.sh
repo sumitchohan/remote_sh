f59a00a9c1af812dd95aaffbc1ae9adbb8c152fc
sumit chohan <sumit@sumits-MacBook-Pro.local>
2018-08-27 06:41:08 -0500

    wip

--- do.sh
+++ do.sh
@@ -783,14 +783,14 @@ ShouldAttack()
 	result="n"
 	if [ "$1" = "1" ]
 	then
-		if  [ "$elixir" -ge "550000" ] || [ "$eg" -ge "1100000" ]
+		if  [ "$elixir" -ge "500000" ] || [ "$eg" -ge "1000000" ]
 		then
 			if [ "$isth10" = "y" ]
 			then	
 				result="y"
 			fi
 		fi 
-		if  [ "$elixir" -ge "800000" ] || [ "$eg" -ge "1600000" ]
+		if  [ "$elixir" -ge "700000" ] || [ "$eg" -ge "1400000" ]
 		then 
 			result="y"
 		fi
@@ -802,7 +802,7 @@ ShouldAttack()
 				result="y"
 			fi
 		fi 
-		if  [ "$elixir" -ge "500000" ] || [ "$eg" -ge "1000000" ]
+		if  [ "$elixir" -ge "400000" ] || [ "$eg" -ge "800000" ]
 		then 
 			result="y"
 		fi 
